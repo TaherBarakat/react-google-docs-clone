8290ee3f346298fe24097215c2f573df8806cf94
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-09 17:58:03 +0300

    setup the server side

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const io = require("socket.io")(3001, {
+     cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+});
+
+io.on("connection", (socket) => {
+     console.log("connected");
+});
