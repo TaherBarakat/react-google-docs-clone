0b9e32dee84611c0e0b1b43c22bbd8563ed8269f
Taher Barakat <taher.barakat12223@gmail.com>
2024-04-12 13:20:51 +0300

    saving the data into a mongodb database

--- client/src/components/TextEditor.jsx
+++ client/src/components/TextEditor.jsx
@@ -3,6 +3,7 @@ import "quill/dist/quill.snow.css";
 import { useCallback, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+const SAVE_INTERVAL_MS = 5000;
 const TOOLBAR_OPTIONS = [
      [{ header: [1, 2, 3, 4, 5, 6, false] }],
      [{ font: [] }],
@@ -27,16 +28,34 @@ export default function TextEditor() {
           };
      }, []);
 
+     // S:load-document,get-document
      useEffect(() => {
           if (socket == null || quill == null) return;
 
           socket.once("load-document", (documents) => {
+               console.log(documents);
                quill.setContents(documents);
                quill.enable();
           });
           socket.emit("get-document", documentId);
      }, [quill, socket, documentId]);
 
+     // S:save-content
+     useEffect(() => {
+          if (socket == null || quill == null) return;
+
+          const interval = setInterval(() => {
+               socket.emit("save-content", quill.getContents());
+               // console.log(quill.getContents().ops[0].insert);
+               // console.log("new insert");
+
+               return () => {
+                    clearInterval(interval);
+               };
+          }, 3000);
+     }, [quill, socket]);
+
+     // S:send-changes > delta , Q:text-change > delta
      useEffect(() => {
           if (socket == null || quill == null) return;
           const handler = (delta, oldDelta, source) => {
@@ -48,6 +67,7 @@ export default function TextEditor() {
           return () => quill.off("text-change", handler);
      }, [quill, socket]);
 
+     // S:receive-changes
      useEffect(() => {
           if (socket == null || quill == null) return;
           const handler = (delta) => {
